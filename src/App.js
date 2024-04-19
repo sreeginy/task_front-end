78fa66c3b28763524e1d98eb36285c35b4d4e92a
Sreeginy <sreeginy98@gmail.com>
2024-04-19 12:30:17 +0530

    create FAQs component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TopNavigation from './components/TopNavigation/TopNavigation';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Frame499 from './components/Frame499/Frame499';
+import FAQs from './components/FAQs/FAQs';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Hero />
       <About />
       <Frame499 />
+      <FAQs />
 
     </div>
 
---
+++ src/components/FAQs/FAQs.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import './FAQs.css'
+import { useState } from 'react'
+
+
+
+const FAQs = () => {
+    const [selected, setSelected] = useState(null)
+
+    const toggle = (i) => {
+        if(selected === i) {
+            return setSelected(null)
+        }
+
+        setSelected(i)
+    }
+
+
+  return (
+
+    <section className="FAQs section" id="FAQS">
+    <h3 className="section_title">Frequently asked questions</h3>
+
+    <div className='ExpandableCard'>
+        <div className="accordion">
+
+            {data.map((item, i) => (
+             <div className="item">   
+                <div className="title" onClick={() => toggle(i)}> 
+                <h4>{item.question}</h4>
+                <span>{selected === i ? '-' : '+'}</span>
+                </div>
+                <div className={selected === i ? 'content show' : 'content'}>{item.answer}</div>
+
+             </div>   
+            ))}
+
+        </div>
+      
+    </div>
+
+    </section>
+  )
+}
+
+const data = [
+    {
+        question:"Lorem ipsum dolor sit amet consectetur. Leo at sit eu libero?",
+        answer:
+            'Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum. Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus sagittis tortor integer.',
+    },
+
+    {
+        question:"Lorem ipsum dolor sit amet consectetur. Tortor scelerisque integer?",
+        answer:
+            'Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum. Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus sagittis tortor integer.',
+    },
+    {
+        question:"Lorem ipsum dolor sit amet consectetur. Faucibus scelerisque nunc?",
+        answer:
+            'Lorem ipsum dolor sit amet consectetur. Faucibus commodo suscipit id ipsum. Elementum ultrices nulla faucibus odio est sed aliquam. Sapien massa morbi risus sagittis tortor integer.',
+    }
+]
+
+export default FAQs
